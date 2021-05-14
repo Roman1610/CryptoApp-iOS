90e0196f486cabf408887f4eabfdf258e9b557a6
Roman <r.uvarov@involta.ru>
2021-05-14 20:27:15 +0300

    Init

---
+++ CurrencyApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CurrencyApp
+//
+//  Created by Roman Fullstack on 14.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
